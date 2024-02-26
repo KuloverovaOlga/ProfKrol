ffcddd03731822df5ccc628c13405d120cc234bc
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2024-02-26 15:24:20 +0400

    add header

--- src/js/dev/kuloverova.js
+++ src/js/dev/kuloverova.js
@@ -18,6 +18,9 @@ document.addEventListener('DOMContentLoaded', () => {
   try {
     phoneMask();
   } catch {}
+  try {
+    dropdown();
+  } catch {}
   
 });
 
@@ -26,4 +29,61 @@ function phoneMask() {
   mask.mask($('.phone-mask'));
 }
 
+function dropdown() {
+  const dropdowns = document.querySelectorAll('.dropdown');
 
+  dropdowns.forEach((dropdown) => {
+    const dropdownLink = dropdown.querySelector('.header__nav-item-link-wrapper');
+    const dropdownInner = dropdown.querySelector('.header__dropdown-inner');
+
+    $(dropdownInner).hide();
+
+    function dropdownClick() {
+      dropdown.classList.toggle('active');
+      $(dropdownInner).slideToggle();
+    }
+
+    function showContent() {
+      if (window.innerWidth < 768) {
+        // $(dropdownInner).hide();
+        dropdownLink.addEventListener('click', dropdownClick);
+        dropdown.classList.remove('active');
+      } else {
+        $(dropdownInner).hide();
+        dropdownLink.removeEventListener('click', dropdownClick);
+      }
+    }
+
+    showContent();
+    window.addEventListener('resize', showContent);
+  });
+
+  const burger = document.querySelector('.header__mob-burger');
+  const headerInner = document.querySelector('.header__inner');
+  const closeBtn = document.querySelector('.header__logo-close-btn');
+
+  burger.addEventListener('click', () => {
+    if (!burger.classList.contains('active')) {
+      burger.classList.add('active');
+      headerInner.classList.add('active');
+      document.body.style.overflow = 'hidden';
+     
+    } 
+  });
+  closeBtn.addEventListener('click', () => {
+    if (burger.classList.contains('active')) {
+      headerInner.classList.remove('active');
+      burger.classList.remove('active');
+      document.body.style.overflow = '';
+     
+    }
+  });
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+      headerInner.classList.remove('active');
+      burger.classList.remove('active');
+      document.body.style.overflow = '';
+    }
+  });
+}
